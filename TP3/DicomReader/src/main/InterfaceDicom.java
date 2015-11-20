257ba67cfa6a9345c48c47a7f5f2f32a7bdd7ff6
Alexandre Sayal <alexandresayal@gmail.com>
2015-11-20 22:50:10 +0000

    Testing Eclipse Comm

--- TP3/DicomReader/src/main/InterfaceDicom.java
+++ TP3/DicomReader/src/main/InterfaceDicom.java
@@ -26,7 +26,7 @@ public class InterfaceDicom extends javax.swing.JFrame implements ListSelectionL
 	/** Creates new form ExemploDicomDir */
 	DefaultListSelectionModel list;
 	public InterfaceDicom() {
-		initComponents();
+		initComponents(); //Do stuff
 	}
 
 	/** This method is called from within the constructor to
