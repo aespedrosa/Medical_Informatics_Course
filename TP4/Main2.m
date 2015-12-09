186692f04ca71ffc34a2fbf86b0eb0e825e20a6e
Alexandre Sayal <alexandresayal@gmail.com>
2015-12-09 17:36:33 +0000

    Tarde 9 Dez

--- TP4/Main2.m
+++ TP4/Main2.m
@@ -2,13 +2,16 @@ clear, clc;
 close all;
 
 %% Configs
-display = true;
+display = false;
 
 %% Load ECG
-fs = 1000;
+fs = 250;
 
-ecg = load('data/a.dat');
-ecg = ecg(:,3);
+% ecg = load('data/ecgPVC.dat');
+
+load 'DATPVC/DPVC_106.mat';
+
+ecg = DAT.ecg(30000:40000);
 
 N = length(ecg);
 
@@ -28,7 +31,7 @@ fc = wc/(0.5*fs); %Normalized cut-off freq
 e1 = filtfilt(b , a , e0);
 
 if display
-    figure(1)
+    figure()
     plot(t,e0,t,e1,'r','LineWidth',1.5)
     title(' LOW PASS FILTER ','FontSize',14)
     zoom on
@@ -43,12 +46,13 @@ fc = wc/(0.5*fs);
 e2 = filtfilt( b , a , e1 );
 
 if display
-    figure(2)
+    figure()
     plot(t,e0,t,e1,'r:',t,e2,'g','LineWidth',1.5)
     title(' HIGH+LOW PASS FILTER ','FontSize',14)
     zoom on
 %     pause
 end
+
 
 %% Difference + Square
 e3 = diff(e2);
@@ -56,13 +60,13 @@ e3(end+1) = e2(end);
 
 e4 = 50*e3.^2;
 if display
-    figure(3)
+    figure()
     plot(t,ecg,t,mean_ecg+e2,'g:',t,e3,'m:','LineWidth',1.5)
     title(' DIFFERENTIATION+SQUARE ','FontSize',14)
     zoom on
     %hold on
 %     pause
-    figure(4)
+    figure()
     plot(t,e4,'r','LineWidth',1.5)
     title(' DIFFERENTIATION+SQUARE ','FontSize',14)
     zoom on
@@ -80,34 +84,47 @@ a = 1;
 e5= 10*filtfilt(b,a, e4);
 
 if display
-    figure(1)
+    figure()
     plot(t,e0,t,e5,'r','LineWidth',1.5)
     title(' MOVING AVERAGE FILTER ','FontSize',14)
     zoom on
-%     pause
 end
 
-%% Find R Peaks
+%% Find R Peaks 
+Rindexes = [];
+threshold = 0.5 * mean(e5);
 
-max_value = max(e5);
-min_value = min(e5);
+for i=2:length(e5)-1
+   if (e5(i) > threshold && e5(i-1) < e5(i) && e5(i+1) < e5(i))
+       Rindexes = [Rindexes i];
+   end
+end
 
-threshold = 0.65;
-
-e6 = e5;
-e6(e6<threshold*max_value) = 0;
-
-d = [ 0 ; diff(e6)];
-d(d < max(d)/2 ) = 0;
-Rindexes = find(d>0);
-
-%%
 figure()
-    plot(t,e2)
+    plot(t,e5)
     hold on
-    plot(t(Rindexes),d(Rindexes),'o')
+    plot(t(Rindexes),e5(Rindexes),'o')
 
 BPM = ( length(Rindexes)*60 ) / max(t);
 
 fprintf('BPM: %0.2f bpm \n',BPM);
 
+%% Find PVC
+
+D = diff(Rindexes);
+
+mean_dist = mean(D);
+
+PVCindexes = Rindexes([ false D<mean_dist*0.8 ]);
+
+figure()
+    plot(t,e5, ...
+        t(Rindexes),e5(Rindexes),'o',...
+        t(PVCindexes),e5(PVCindexes),'go');
+    legend('Energy','R Peaks','PVC');
+    title('PVC Detection');
+    xlabel('Time (s)')
+    grid on
+    
+    
+    
